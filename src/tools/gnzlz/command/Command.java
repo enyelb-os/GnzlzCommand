2337ec2c383b1a9c9e7b13424ac9c6f01e3dc570
enyelb-os <enyelb.gnzlz@gmail.com>
2023-01-03 19:58:34 -0400

    update init

--- src/tools/gnzlz/command/Command.java
+++ src/tools/gnzlz/command/Command.java
@@ -1,4 +1,4 @@
-package command;
+package tools.gnzlz.command;
 
 import java.util.ArrayList;
 
--- src/tools/gnzlz/command/ForCommand.java
+++ src/tools/gnzlz/command/ForCommand.java
@@ -1,4 +1,4 @@
-package command;
+package tools.gnzlz.command;
 
 @FunctionalInterface
 public interface ForCommand {
