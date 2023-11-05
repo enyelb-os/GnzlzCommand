eeeeb06e6b2bd8430f265f0b9e2ada3d29c99e82
enyelb-os <enyelb.gnzlz@gmail.com>
2023-11-04 21:28:31 -0400

    update

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module tools.gnzlz.command {
+    exports tools.gnzlz.command.command;
+    exports tools.gnzlz.command.command.object;
+    exports tools.gnzlz.command.command.functional;
+    exports tools.gnzlz.command.command.type;
+    exports tools.gnzlz.command.result;
+    exports tools.gnzlz.command.group;
+    exports tools.gnzlz.command.init;
+    exports tools.gnzlz.command.process;
+    exports tools.gnzlz.command.functional;
+}
