08f40bfd278a3226b12826177443e47c2a90641b
enyelb-os <enyelb.gnzlz@gmail.com>
2023-11-29 12:50:20 -0400

    update

--- src/tools/gnzlz/command/ansi/Color.java
+++ src/tools/gnzlz/command/ansi/Color.java
@@ -2,40 +2,87 @@ package tools.gnzlz.command.ansi;
 
 public enum Color {
 
+    /**
+     * BLACK
+     */
     BLACK(Command.BLACK,Command.BLACK_BG),
+
+    /**
+     * RED
+     */
     RED(Command.RED,Command.RED_BG),
+
+    /**
+     * GREEN
+     */
     GREEN(Command.GREEN,Command.GREEN_BG),
+
+    /**
+     * YELLOW
+     */
     YELLOW(Command.YELLOW,Command.YELLOW_BG),
+
+    /**
+     * BLUE
+     */
     BLUE(Command.BLUE,Command.BLUE_BG),
+
+    /**
+     * PURPLE
+     */
     PURPLE(Command.PURPLE,Command.PURPLE_BG),
+
+    /**
+     * CYAN
+     */
     CYAN(Command.CYAN,Command.CYAN_BG),
+
+    /**
+     * WHITE
+     */
     WHITE(Command.WHITE,Command.WHITE_BG);
 
-    protected final Command foreground;
-    protected final Command background;
+    /**
+     * BLACK
+     */
+    private final Command foreground;
 
-    private Color(Command foreground, Command background){
+    /**
+     * BLACK
+     */
+    private final Command background;
+
+    /**
+     * Color
+     * @param foreground fore
+     * @param background back
+     */
+    Color(Command foreground, Command background){
         this.foreground = foreground;
         this.background = background;
     }
 
+    /**
+     * print
+     * @param text text
+     */
     public String print(Object text) {
         return foreground.toString().concat(text.toString()).concat(Command.RESET.toString());
     }
 
-
-    public String print(Color background,Object text) {
+    /**
+     * print
+     * @param background b
+     * @param text t
+     */
+    public String print(Color background, Object text) {
         return background.background.toString().concat(this.print(text));
     }
 
-    public String setFG() {
-        return foreground.toString();
-    }
-
-    public String setBG() {
-        return background.toString();
-    }
-
+    /**
+     * set
+     * @param background back
+     */
     public String set(Color background) {
         return background.background.toString().concat(foreground.toString());
     }
--- src/tools/gnzlz/command/ansi/Command.java
+++ src/tools/gnzlz/command/ansi/Command.java
@@ -4,24 +4,74 @@ import tools.gnzlz.command.process.print.PrintCommand;
 
 public enum Command {
 
+    /**
+     * RESET
+     */
     RESET("\u001B[0m",""),
+
+    /**
+     * BLACK
+     */
     BLACK("\u001B[30m",""), BLACK_BG("\u001B[40m",""),
+
+    /**
+     * RED
+     */
     RED("\u001B[31m",""),  RED_BG("\u001B[41m",""),
+
+    /**
+     * GREEN
+     */
     GREEN("\u001B[32m",""), GREEN_BG("\u001B[42m",""),
+
+    /**
+     * YELLOW
+     */
     YELLOW("\u001B[33m",""), YELLOW_BG("\u001B[43m",""),
+
+    /**
+     * BLUE
+     */
     BLUE("\u001B[34m",""), BLUE_BG("\u001B[44m",""),
+
+    /**
+     * PURPLE
+     */
     PURPLE("\u001B[35m",""), PURPLE_BG("\u001B[45m",""),
+
+    /**
+     * CYAN
+     */
     CYAN("\u001B[36m",""),  CYAN_BG("\u001B[46m",""),
+
+    /**
+     * WHITE
+     */
     WHITE("\u001B[37m",""), WHITE_BG("\u001B[47m","");
 
-    protected final String cmd;
-    protected final String terminal;
+    /**
+     * cmd
+     */
+    private final String cmd;
 
-    private Command(String cmd, String terminal){
+    /**
+     * terminal
+     */
+    private final String terminal;
+
+    /**
+     * Command
+     * @param cmd cmd
+     * @param terminal terminal
+     */
+    Command(String cmd, String terminal){
         this.cmd = cmd;
         this.terminal = terminal;
     }
 
+    /**
+     * toString
+     */
     @Override
     public String toString() {
         if (System.getProperty("os.name").contains("Windows")) {
